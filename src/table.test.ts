ece40ef322232f6897ba515de758bd2d8ee710ac
David Siegel <djsiegel@gmail.com>
2023-11-15 21:42:13 -0800

    Tests

--- src/table.test.ts
+++ src/table.test.ts
@@ -27,6 +27,19 @@ const inventoryStaging = glide.table({
   },
 });
 
+describe("app", () => {
+  it("can get tables", async () => {
+    const tables = await app.getTables();
+    expect(tables).toBeDefined();
+    expect(tables?.length).toBeGreaterThan(0);
+  });
+
+  it("can get a table by name", async () => {
+    const table = await app.getTableNamed("Inv - Inventory");
+    expect(table).toBeDefined();
+  });
+});
+
 describe("table", () => {
   jest.setTimeout(60_000);
 
@@ -109,16 +122,5 @@ describe("table", () => {
       data: { columns },
     } = await inventory.getSchema();
     expect(columns).toBeTruthy();
-  });
-
-  it.only("can get tables", async () => {
-    const tables = await app.getTables();
-    expect(tables).toBeDefined();
-    expect(tables?.length).toBeGreaterThan(0);
-  });
-
-  it.only("can get a table by name", async () => {
-    const table = await app.getTableNamed("Inv - Inventory");
-    expect(table).toBeDefined();
   });
 });
