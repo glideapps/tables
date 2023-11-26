0432a9e08e8eb1da7907c5ebbab569ab693588a6
David Siegel <djsiegel@gmail.com>
2023-11-26 12:14:42 -0800

    Bind

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Glide } from "./Glide";
 
 const defaultClient = new Glide();
 
-export const app = defaultClient.app;
-export const table = defaultClient.table;
+export const app = defaultClient.app.bind(defaultClient);
+export const table = defaultClient.table.bind(defaultClient);
 export const getApps = defaultClient.getApps.bind(defaultClient);
 export const getAppNamed = defaultClient.getAppNamed.bind(defaultClient);
