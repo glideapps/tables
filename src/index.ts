03c3f4d3f8cf5e61e5b3f608b443256395b67764
David Siegel <djsiegel@gmail.com>
2024-02-09 07:02:26 -0800

    Export RowOf

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,5 @@ export const app = defaultClient.app.bind(defaultClient);
 export const table = defaultClient.table.bind(defaultClient);
 export const getApps = defaultClient.getApps.bind(defaultClient);
 export const getAppNamed = defaultClient.getAppNamed.bind(defaultClient);
+
+export { RowOf } from "./types";
