bacefa1e5d24e18aa8768f12db04b478cdaf5a33
David Siegel <djsiegel@gmail.com>
2024-03-09 15:33:37 -0800

    Export more type

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ export const getApps = defaultClient.getApps.bind(defaultClient);
 export const getAppNamed = defaultClient.getAppNamed.bind(defaultClient);
 export const withConfig = defaultClient.with.bind(defaultClient);
 
-export { RowOf, AppManifest, ColumnType } from "./types";
+export { RowOf, AppManifest, ColumnType, ColumnSchema } from "./types";
 export { type Table } from "./Table";
 export { type Glide } from "./Glide";
+export { type App } from "./App";
