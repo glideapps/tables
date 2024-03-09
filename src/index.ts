4d0f88779f4bdc47359ee7a8701c049b3f6362f2
David Siegel <dvdsgl@users.noreply.github.com>
2024-03-09 13:59:10 -0800

    Expose some types (#54)
    * Expose Table type
    
    * wip

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,5 @@ export const getApps = defaultClient.getApps.bind(defaultClient);
 export const getAppNamed = defaultClient.getAppNamed.bind(defaultClient);
 export const withConfig = defaultClient.with.bind(defaultClient);
 
-export { RowOf, AppManifest } from "./types";
+export { RowOf, AppManifest, ColumnType } from "./types";
+export { type Table } from "./Table";
